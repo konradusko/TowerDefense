08b52cce68ae0a3fd92b9f937dbf2ac04704ce62
Konrad Bolesławski <42543331+konradusko@users.noreply.github.com>
2019-10-24 18:17:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ const myGameArea = {
             this.enemyPath.x = this.enemyPath.x + this.oneBox;
         };
         this.breakPoints.push(this.enemypathArray.slice(-1)[0]);
-        for (let o = 0; o < 6; o++) {
+        for (let o = 0; o < 5; o++) {
             this.createElement(this.enemyPath.width, this.enemyPath.height, this.enemyPath.x, this.enemyPath.y, this.enemyPath.img, this.enemypathArray);
             this.enemyPath.y = this.enemyPath.y - this.oneBox;
         };
@@ -82,7 +82,7 @@ const myGameArea = {
         };
         this.breakPoints.push(this.enemypathArray.slice(-1)[0]);
         console.log(this.breakPoints)
-        for (let z = 0; z < 11; z++) {
+        for (let z = 0; z < 10; z++) {
             this.createElement(this.enemyPath.width, this.enemyPath.height, this.enemyPath.x, this.enemyPath.y, this.enemyPath.img, this.enemypathArray);
             this.enemyPath.y = this.enemyPath.y + this.oneBox;
         };
@@ -136,13 +136,13 @@ const myGameArea = {
     },
     createEnemy: function () {
 
-        let poziom = 10;
+        let poziom = 1;
         let numberTest = 0;
         this.enemyArray = [];
         for (let i = 0; i < poziom; i++) {
             //tworzenie potworków i dodawanie ich do arraya
             this.createElement(this.enemyOne.width, this.enemyOne.height, this.enemyOne.x, this.enemyOne.y, this.enemyOne.img, this.enemyArray, this.enemyOne.health);
-            this.enemyOne.x = this.enemyOne.x - this.oneBox;
+            this.enemyOne.x = this.enemyOne.x ;
         }
         console.log(this.enemyArray)
 
@@ -153,206 +153,46 @@ const myGameArea = {
         }, 200);
     },
     drawEnemy: function (a) {
+ if(a < this.enemypathArray.length){
+    this.enemyArray.forEach(element => {
+        this.ctx_2.clearRect(0, 0, this.canvas_2.height, this.canvas_2.width)
+        this.ctx_2.beginPath();
+        element.x = this.enemypathArray[a].x  ;
+        element.y = this.enemypathArray[a].y  ;
+        this.ctx_2.fillRect(element.x, element.y, element.width, element.height);
+    });
+ }
+       
+        /*
+        this.ctx_2.clearRect(0, 0, this.canvas_2.height, this.canvas_2.width)
            console.log(this.enemyArray)
            console.log(this.enemypathArray)
-        this.ctx_2.clearRect(0, 0, this.canvas_2.height, this.canvas_2.width);
         if (this.enemyArray.length != 0) {
+            this.ctx_2.clearRect(0, 0, this.canvas_2.height, this.canvas_2.width)
             this.enemyArray.forEach(el => {
-                el.x = this.enemypathArray[a].x + 5 ;
-                el.y = this.enemypathArray[a].y + 5 ;
-                this.ctx_2.fillRect(el.x, el.y, el.width, el.height);
+             el.x = this.enemypathArray[a].x  ;
+                   el.y = this.enemypathArray[a].y  ;
+                   this.ctx_2.fillRect(el.x, el.y, el.width, el.height);
 
-           /*
-                if (el.x <= this.breakPoints[0].x + this.oneBox) {
-                    console.log(1);
-                    el.x = el.x + this.oneBox / 2;
-                    this.ctx_2.fillRect(el.x, el.y, el.width, el.height);
-                } else if (el.y <= this.breakPoints[1].y + this.oneBox && el.x < this.breakPoints[3].x && el.y != this.breakPoints[5].y) {
-                    console.log(2);
-                    el.y = el.y + this.oneBox / 2;
-                    this.ctx_2.fillRect(el.x, el.y, el.width, el.height);
-                } else if (el.x <= this.breakPoints[2].x + this.oneBox && el.y < this.breakPoints[4].x+ this.oneBox ) {
-                    console.log(3);
-                    el.x = el.x + this.oneBox / 2;
-                    //    el.y=el.y - this.oneBox/2;
-                    this.ctx_2.fillRect(el.x, el.y, el.width, el.height);
 
-                } else if (el.y >= this.breakPoints[3].y - this.oneBox / 2 && el.y < this.breakPoints[4].x+this.oneBox) {
-                    console.log(4); 
-                    el.y = el.y - this.oneBox / 2 ;
-                    this.ctx_2.fillRect(el.x, el.y, el.width, el.height);
-
-                } else if (el.x <= this.breakPoints[4].x ) {
-                    console.log(5);
-                    el.x = el.x + this.oneBox / 2;
-                    this.ctx_2.fillRect(el.x, el.y, el.width, el.height);
-                }  else if(el.y <= this.breakPoints[5].y+this.oneBox){
-                    console.log(6);
-                    el.y = el.y + this.oneBox/2;
-                    this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                } else {
-                    this.enemyArray.shift(el)
-                    console.log(this.enemyArray)
-                }
-
-                /*
-                this.promise = new Promise((res,rej) =>{
-                    console.log(this.breakPoints[3].y)
-                 if(el.x <= this.breakPoints[0].x +this.oneBox){
-                  
-                    el.x = el.x +this.oneBox/2;
-                    this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                }else if(el.y <= this.breakPoints[1].y + this.oneBox &&el.x < this.breakPoints[3].x && el.y != this.breakPoints[5].y ){
-                   
-                    el.y = el.y + this.oneBox/2;
-                    this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                }else if(el.x <= this.breakPoints[2].x + this.oneBox){
-                    console.log("pewnie kurwa tez sie robi")
-                    el.x=el.x +this.oneBox/2;
-                    this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                }else{
-                    res();
-                }
-                })
-                this.promise.then(() =>{
-                     console.log(el.y)
-                    switch(el.y){
-                        default:
-                            let xdtest = 1;
-                            break;
-                        case 1:
-                            break;
-                    }
-
-                   if(el.y >=  this.breakPoints[3].y -this.oneBox/2){
-                       
-                    
-                        el.y = el.y - this.oneBox/2 ;
-                        // console.log( el.y = el.y - 20)
-                  
-                          this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                    }else if(el.x <= this.breakPoints[4].x + this.oneBox){
-                        console.log("robi sie ? ")
-                        console.log(5);
-                          el.x = el.x +this.oneBox/2;
-                          this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                    } 
-                    
-                })
-                /*
-                        switch(el.x){
-                            default:
-                                if(el.x <= this.breakPoints[0].x +this.oneBox && el.x <= 5){
-                                    el.x = el.x+this.oneBox/2;
-                                   // this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                                }
-                                
-                            console.log("xd");
-                            break;
-                            case 5:
-                                console.log("MHHMHMHMHM")
-                          
-                                console.log("mniejsze?")
-                                setTimeout(() => {
-                                    while(el.x <= this.breakPoints[0].x+this.oneBox)
-                                    {
-                                    el.x = el.x+this.oneBox/2;
-                                    this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                                    console.log("while is coming")
-                                }
-                                }, 1000);
-                            
-                        
-                                
-                                    break;
-                        }
-                        */
-
-                // console.log(this.breakPoints[3].y)
-                //console.log(this.breakPoints[4].x)
-                //console.log(this.breakPoints[0].x)
-                /*
-                this.promise = new Promise((resolve,reject) =>{
-                    console.log("tutaj promisek");
-                    if(el.x <= this.breakPoints[0].x +this.oneBox){
-                        console.log("resolve")
-                        resolve();
-                    }
-                    while (el.x <= 0){
-                        reject();
-                      
-                     
-                    }
-                   
-                });
-                this.promise.then(() =>{
-                    el.x = el.x + this.oneBox/2
-                    this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                })
-                this.promise.catch(() =>{
-                    el.x = el.x + this.oneBox
-                    console.log("xddd")
-                })
-                */
-                /*
-                    this.promise.then( () =>{
-                        el.x = el.x+this.oneBox/2;
-                        this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                        this.promise = new Promise((resolve,reject) =>{
-                            if(el.y<= this.breakPoints[1].y +this.oneBox){
-                                resolve();
-                            }
-                        })
-                        this.promise.then(() =>{
-                            el.y = el.y+this.oneBox/2;
-                            this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                        })
-                    })
-                    */
-                /*
-                    if(el.x <= this.breakPoints[0].x +this.oneBox){
-                        console.log(1);
-                        el.x = el.x+this.oneBox/2;
-                        this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                    }else if(el.y <= this.breakPoints[1].y + this.oneBox && el.x < this.breakPoints[3].x && el.y != this.breakPoints[5].y){
-                        console.log(2);
-                        el.y = el.y+this.oneBox/2;
-                        this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                    }else if(el.x <= this.breakPoints[2].x + this.oneBox && el.x < this.breakPoints[5].y+this.oneBox && el.x != this.breakPoints[4].x&& el.x+40 != this.breakPoints[4].x){
-                        console.log(3);
-                        el.x = el.x + this.oneBox/2;
-                    //    el.y=el.y - this.oneBox/2;
-                        this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                    
-                    } else if(el.y >=  this.breakPoints[3].y -this.oneBox/2 && el.x < this.breakPoints[4].x && el.x != this.breakPoints[4].x){
-                        console.log(4);
-                       // console.log("robi sie to ?")
-                       el.y = el.y - this.oneBox/2 - 5;
-                      // console.log( el.y = el.y - 20)
-                
-                        this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                     
-                    }else if(el.x <= this.breakPoints[4].x + this.oneBox&& el.y === this.breakPoints[4].y){
-                        console.log(5);
-                          el.x = el.x +this.oneBox/2;
-                          this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                    } else if(el.y <= this.breakPoints[5].y+this.oneBox){
-                        console.log(6);
-                        el.y = el.y + this.oneBox/2;
-                        this.ctx_2.fillRect(el.x,el.y,el.width,el.height);
-                    } else {
-                        this.enemyArray.shift(el)
-                        console.log(this.enemyArray)
-                    } 
-                    */
-            });
+            //     if(el.x < 5){
+            //        el.x = el.x +40;
+            //        console.log(this.enemyArray)
+            //     }else{
+            //         el.x = el.x = this.enemypathArray[a].x;
+            //         this.ctx_2.fillRect(el.x, el.y, el.width, el.height);
+            //     }
+            //    //     el.x = this.enemypathArray[a].x + 5 ;
+                //    el.y = this.enemypathArray[a].y + 5 ;
+              
+                  });
 
         } else {
             console.log("array is empty")
             clearInterval(this.interval);
         }
 
-
+*/
 
     },
     createElement: function (width, height, x, y, img, path, life) {
@@ -378,15 +218,3 @@ const myGameArea = {
 
 }
 myGameArea.start();
-/*
-const myGameArea_2 = {
-    canvas_2: document.getElementById("canvas2"),
-    ctx_2: undefined,
-    init:function(){
-this.ctx_2= this.canvas_2.getContext("2d");
-this.canvas_2.width = 800;
-this.canvas_2.height = 600;
-  this.ctx_2.fillRect(40,40,40,40)
-    }
-}
-*/
